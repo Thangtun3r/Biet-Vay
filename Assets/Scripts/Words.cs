5332b831e38dddcbba8d1c0a14936b657e223160
Thangtuner <voquangthang04@gmail.com>
2025-06-22 19:52:04 +0700

    Done on words spacing out to indicate for the player where the word will be putting in

--- Assets/Scripts/Words.cs
+++ Assets/Scripts/Words.cs
@@ -1,28 +1,44 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-public class Words : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
-{
-    Transform parentAfterDrag;
-    public void OnPointerDown(PointerEventData eventData)
-    {
-       
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = Input.mousePosition;
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    { 
-        transform.SetParent(parentAfterDrag);
-    }
-}
+using System;
+        using UnityEngine;
+        using UnityEngine.EventSystems;
+        
+        public class Words : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+        {
+            Transform parentAfterDrag;
+            private Collider2D _collider2D;
+            public Collider2D detectorCollider;
+        
+            private void Awake()
+            {
+                _collider2D = GetComponent<Collider2D>();
+            }
+        
+            public void OnPointerDown(PointerEventData eventData)
+            {
+        
+            }
+        
+            public void OnBeginDrag(PointerEventData eventData)
+            {
+                parentAfterDrag = transform.parent;
+                transform.SetParent(transform.root);
+                transform.SetAsLastSibling();
+                if (_collider2D != null)
+                {
+                    _collider2D.enabled = false; 
+                    detectorCollider.enabled = true;
+                }
+            }
+        
+            public void OnDrag(PointerEventData eventData)
+            {
+                transform.position = Input.mousePosition;
+            }
+        
+            public void OnEndDrag(PointerEventData eventData)
+            {
+                transform.SetParent(parentAfterDrag);
+                _collider2D.enabled = true; 
+                detectorCollider.enabled = false;
+            }
+        }
