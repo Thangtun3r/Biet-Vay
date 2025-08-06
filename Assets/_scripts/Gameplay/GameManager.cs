2551c8c1f46ff8551e8d41da832eda4546547eb7
Thangtuner <voquangthang04@gmail.com>
2025-08-07 02:48:57 +0700

    Add simple first person movement

---
+++ Assets/_scripts/Gameplay/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This will be the centralized manager for the game handeling event to communnicate between Yarn and Unity.
+public class GameManager : MonoBehaviour
+{
+    
+}
