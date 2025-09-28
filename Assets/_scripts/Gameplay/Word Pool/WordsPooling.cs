c7e1debb3ad2ef977e35db369fefd1ba5d9cd0dc
wiwenid <voquangthang04@gmail.com>
2025-09-29 03:36:26 +0700

    added the flow of vignette 3 and fix minor bug

--- Assets/_scripts/Gameplay/Word Pool/WordsPooling.cs
+++ Assets/_scripts/Gameplay/Word Pool/WordsPooling.cs
@@ -89,6 +89,7 @@ public class WordsPooling : MonoBehaviour
             {
                 obj.SetActive(false);
                 obj.transform.SetParent(transform, false);
+                
             }
         }
     }
