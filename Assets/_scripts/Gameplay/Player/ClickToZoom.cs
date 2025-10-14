731fc1936890682cc559afda7a1f234941b27cf4
Thangtuner <voquangthang04@gmail.com>
2025-10-14 20:15:58 +0700

    fix camera bug and vignette 5

--- Assets/_scripts/Gameplay/Player/ClickToZoom.cs
+++ Assets/_scripts/Gameplay/Player/ClickToZoom.cs
@@ -1,21 +1,18 @@
-/*using UnityEngine;
+using UnityEngine;
 using Cinemachine;
 
-[RequireComponent(typeof(CinemachineBrain))]
 public class HoldToZoom : MonoBehaviour
 {
-    CinemachineBrain brain;
+    [Header("References")]
+    public CinemachineVirtualCamera vcam;
+
+    [Header("Zoom Settings")]
     public float zoomedFOV = 30f;
     public float normalFOV = 60f;
     public float smooth = 10f;
 
-    void Awake() => brain = GetComponent<CinemachineBrain>();
-
     void LateUpdate()
     {
-        if (brain == null || brain.IsBlending) return; // <-- let blends happen
-
-        var vcam = brain.ActiveVirtualCamera as CinemachineVirtualCamera;
         if (vcam == null) return;
 
         float targetFOV = Input.GetMouseButton(1) ? zoomedFOV : normalFOV;
@@ -24,4 +21,4 @@ public class HoldToZoom : MonoBehaviour
         lens.FieldOfView = Mathf.MoveTowards(lens.FieldOfView, targetFOV, smooth * Time.deltaTime);
         vcam.m_Lens = lens;
     }
-}*/
+}
